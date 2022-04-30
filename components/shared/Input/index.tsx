e7bc72114ff164ef42ee201889a36106404f8ea4
Ansh Saini <anshsaini625@gmail.com>
2022-04-30 11:03:55 +0530

    style home screen and add join room feature

--- components/shared/Input/index.tsx
+++ components/shared/Input/index.tsx
@@ -1,25 +1,36 @@
 import clsx from "clsx";
-import React from "react";
+import React, { forwardRef } from "react";
 import styles from "./Input.module.css";
 
 type Props = {
   children?: React.ReactNode;
   label?: string;
+  classes?: {
+    input?: string;
+  };
 } & React.InputHTMLAttributes<HTMLInputElement>;
 
-const Input = ({ children, className, label, ...props }: Props) => {
-  return (
-    <div className={clsx(styles.root, className)}>
-      {label && (
-        <label htmlFor={props.name} className={styles.label}>
-          {label}
-        </label>
-      )}
-      <input className={styles.input} {...props}>
-        {children}
-      </input>
-    </div>
-  );
-};
+const Input = forwardRef(
+  ({ children, classes, className, label, ...props }: Props, ref) => {
+    return (
+      <div className={clsx(styles.root, className)}>
+        {label && (
+          <label htmlFor={props.name} className={styles.label}>
+            {label}
+          </label>
+        )}
+        <input
+          // @ts-expect-error Can't seem to fix this
+          ref={ref}
+          className={clsx(styles.input, classes?.input)}
+          {...props}
+        >
+          {children}
+        </input>
+      </div>
+    );
+  }
+);
 
+Input.displayName = "Input";
 export default Input;
