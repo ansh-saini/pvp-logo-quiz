30df06e6b350bab2a92890d15f6b8b603b1a1512
Ansh Saini <anshsaini625@gmail.com>
2022-04-30 11:15:48 +0530

    add styling, copy code feature in lobby screen

--- components/Lobby/index.tsx
+++ components/Lobby/index.tsx
@@ -1,24 +1,39 @@
+import PageLayout from "components/PageLayout";
+import Button from "components/shared/Button";
+import Input from "components/shared/Input";
 import Head from "next/head";
-import React from "react";
+import React, { useRef, useState } from "react";
 import { ParsedRoom } from "utils/models";
 import styles from "./Lobby.module.css";
 
 type Props = { room: ParsedRoom };
 
 const Lobby = ({ room }: Props) => {
+  const inputRef = useRef<HTMLInputElement>();
+  const [copied, setCopied] = useState(false);
+
+  const copyText = () => {
+    setCopied(true);
+    navigator.clipboard.writeText(room.code);
+    setTimeout(() => {
+      setCopied(false);
+    }, 3000);
+  };
+
   return (
-    <>
+    <PageLayout>
       <Head>
         <title>Lobby | {room.code}</title>
       </Head>
+      <h1>Please wait while the other players join.</h1>
 
-      <div className={styles.container}>
-        <h1>
-          Please wait while the other players join. Share room code. {room.code}
-        </h1>
-        <button>Join game</button>
-      </div>
-    </>
+      <h1>Share room code</h1>
+
+      <Input ref={inputRef} value={room.code} className={styles.input} />
+      <Button onClick={copyText}>
+        {copied ? "Copied!" : "Click to copy code"}
+      </Button>
+    </PageLayout>
   );
 };
 
