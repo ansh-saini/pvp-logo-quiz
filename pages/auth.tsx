e8946ebeefdd9abdc72902e1d06e33e6279b9510
Ansh Saini <anshsaini625@gmail.com>
2022-05-03 16:28:10 +0530

    Implement signup

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -5,18 +5,37 @@ import { appwrite } from "global/appwrite";
 import type { NextPage } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "styles/auth.module.css";
+import { API } from "utils/api";
+
+enum MODE {
+  LOGIN,
+  SIGNUP,
+}
 
 const Auth: NextPage = () => {
   const router = useRouter();
 
-  const [formState, setFormState] = useState({
-    email: "a",
-    password: "a",
-  });
+  const [mode, setMode] = useState(MODE.LOGIN);
 
+  const [formState, setFormState] = useState({
+    email: "",
+    password: "",
+    name: "",
+  });
   const [error, setError] = useState("");
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const res = await API.account.get();
+      if (res.isOK) {
+        router.replace("/");
+      }
+    };
+
+    if (router.isReady) checkAuth();
+  }, [router]);
 
   const handleChange = (e: any) => {
     setFormState((prevState) => ({
@@ -27,6 +46,15 @@ const Auth: NextPage = () => {
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
+
+    if (isSignUpMode) {
+      createUser(createSession);
+    } else {
+      createSession();
+    }
+  };
+
+  const createSession = () => {
     const promise = appwrite.account.createSession(
       formState.email,
       formState.password
@@ -37,24 +65,27 @@ const Auth: NextPage = () => {
         setError("");
         router.push("/");
       },
-      function () {
-        setError("Invalid Credentials");
+      function (e) {
+        if (e.response?.message) setError(e.response.message);
+        else setError("Invalid Credentials");
       }
     );
   };
 
-  // const createUser = () => {
-  //   appwrite.account
-  //     .create("unique()", "me@example.com", "password", "Jane Doe")
-  //     .then(
-  //       (response: any) => {
-  //         console.log(response);
-  //       },
-  //       (error: any) => {
-  //         console.log(error);
-  //       }
-  //     );
-  // };
+  const createUser = (onSuccess: () => void) => {
+    const { email, password, name } = formState;
+
+    return appwrite.account.create("unique()", email, password, name).then(
+      (response: any) => {
+        onSuccess();
+      },
+      (e: any) => {
+        if (e.response?.message) setError(e.response.message);
+      }
+    );
+  };
+
+  const isSignUpMode = mode === MODE.SIGNUP;
 
   return (
     <>
@@ -65,10 +96,22 @@ const Auth: NextPage = () => {
 
       <PageLayout>
         <form className={styles.form} onSubmit={handleSubmit}>
+          {isSignUpMode && (
+            <div className="form-group">
+              <Input
+                label="Name"
+                id="name"
+                name="name"
+                value={formState.name}
+                onChange={handleChange}
+                required
+              />
+            </div>
+          )}
           <div className="form-group">
             <Input
               label="Email"
-              type="text"
+              type="email"
               id="email"
               name="email"
               value={formState.email}
@@ -89,7 +132,19 @@ const Auth: NextPage = () => {
           </div>
           {error && <p className={styles.error}>{error}</p>}
           <div className="form-group">
-            <Button type="submit">Log In</Button>
+            <Button type="submit">{isSignUpMode ? "Sign Up" : "Log In"}</Button>
+            <Button
+              type="button"
+              color="danger"
+              onClick={() => {
+                setMode(isSignUpMode ? MODE.LOGIN : MODE.SIGNUP);
+              }}
+              style={{ marginTop: 8 }}
+            >
+              {isSignUpMode
+                ? "Already have an account?"
+                : "Don't Have an account?"}
+            </Button>
           </div>
         </form>
       </PageLayout>
