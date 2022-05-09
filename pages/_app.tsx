55775df42588af47376c227b2d4b8a8c133d21c1
Ansh Saini <anshsaini625@gmail.com>
2022-05-09 15:55:04 +0530

    add meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,9 +6,35 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        {/* <!-- Primary Meta Tags --> */}
         <title>PvP Logo Quiz</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="PvP Logo Quiz" />
+
+        <meta name="title" content="PvP Logo Quiz" />
+        <meta name="description" content="Play Logo Quiz with Friends!" />
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://pvp-logo-quiz.vercel.app/" />
+        <meta property="og:title" content="PvP Logo Quiz" />
+        <meta
+          property="og:description"
+          content="Play Logo Quiz with Friends!"
+        />
+        <meta property="og:image" content="/Cover.png" />
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://pvp-logo-quiz.vercel.app/"
+        />
+        <meta property="twitter:title" content="PvP Logo Quiz" />
+        <meta
+          property="twitter:description"
+          content="Play Logo Quiz with Friends!"
+        />
+        <meta property="twitter:image" content="/Cover.png" />
       </Head>
       <Component {...pageProps} />
     </>
